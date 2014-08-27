32b6b061a2513c6a9d9b1d76aa004c555fa451fc
Eric Range <mail@ericrange.de>
2014-08-27 23:30:37 +0200

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,11 +16,24 @@ module.exports = function(grunt) {
         files: "content/themes/ericrange-v1/assets/scss/**/*.scss",
         tasks: ["sass"]
       }
+    },
+    uglify: {
+      dist: {
+        options: { },
+        files: {
+          "content/themes/ericrange-v1/assets/js/scripts.min.js": [
+            "content/themes/ericrange-v1/assets/js/vendor/*.js"
+          ],
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks("grunt-contrib-sass");
   grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
   grunt.registerTask("default", ["watch"]);
   grunt.registerTask("scss", ["sass"]);
+  grunt.registerTask("js", ["uglify"]);
 };

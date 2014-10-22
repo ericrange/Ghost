3acdd5ea8941c8c5a46e8aac91c5ca0ce4dbfdac
Eric Range <mail@ericrange.de>
2014-10-22 17:17:41 +0200

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
     watch: {
       css: {
         files: "content/themes/ericrange-v1/assets/scss/**/*.scss",
-        tasks: ["sass", "autoprefixer"]
+        tasks: ["sass", "autoprefixer", "cssmin"]
       }
     },
     uglify: {
@@ -36,12 +36,21 @@ module.exports = function(grunt) {
                 "content/themes/ericrange-v1/assets/css/defaults.css"
           }
       }
+    },
+    cssmin: {
+      dist: {
+        files: {
+          "content/themes/ericrange-v1/assets/css/prefixed.defaults.min.css": 
+            ["content/themes/ericrange-v1/assets/css/prefixed.defaults.css"]
+        }
+      }
     }
   });
   grunt.loadNpmTasks("grunt-autoprefixer");
   grunt.loadNpmTasks("grunt-contrib-sass");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
   grunt.registerTask("default", ["watch"]);
   grunt.registerTask("scss", ["sass"]);
--- _install-custom-packages.sh
+++ _install-custom-packages.sh
@@ -3,3 +3,4 @@ npm install grunt-contrib-sass --save-dev
 npm install mysql --save-dev
 npm install grunt-contrib-uglify --save-dev
 npm install grunt-autoprefixer --save-dev
+npm install grunt-contrib-cssmin --save-dev

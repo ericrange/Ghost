3f350698d6d5ca2b87555a4dc5bec044e33fabdb
Eric Range <mail@eric-range.de>
2015-06-13 17:15:55 +0200

    Update

--- gruntfile.js
+++ gruntfile.js
@@ -13,11 +13,22 @@ module.exports = function (grunt) {
 				files: ['scss/styles.scss'],
 				tasks: ['sass']
 			},
+		},
+		cssmin: {
+			options: {
+
+			},
+			target: {
+				files: {
+					'css/styles.css': ['css/styles.css']
+				}
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 	grunt.registerTask("default", ["watch"]);
 };
